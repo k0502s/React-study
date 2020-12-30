23cff8abc3ad56bd9fc03b1974e513c8b7321b05
k0502s <k0502s@naver.com>
2020-12-30 16:01:34 +0900

    React .js 공부
    컴포넌트 코드 실습

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Subject from "./component/Subject"
+import './App.css';
+
+class App extends Component {
+  render (){
+return (    
+    
+    <div className="App">
+      <Subject title="WEB_분리 Test" sub="Hi, My name is jin seok."></Subject> 
+    
+    </div>
+    
+  );
+ }
+}
+
+
+
+export default App;
