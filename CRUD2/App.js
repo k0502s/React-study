d3f2ed3d8aec7476867a397711928e41d2d2d872
k0502s <k0502@naver.com>
2021-01-04 15:57:23 +0900

    React.js
    CRUD Update 구현 완료 코드

--- CRUD2/App.js
+++ CRUD2/App.js
@@ -6,6 +6,7 @@ import Control from "./components/Control"
 import CreateContent from "./components/CreateContent"
 import UpdateContent from "./components/UpdateContent"
 import './App.css';
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -48,28 +49,32 @@ class App extends Component {
       else if(this.state.mode === 'create'){
         _article = <CreateContent onSubmit={function(_title,_desc){
             this.max_content_id = this.max_content_id+1;
-            var _contents = this.state.contents.concat(
-           {id:this.max_content_id, title:_title, desc:_desc}
-            )
+            var _contents = Array.from(this.state.contents);
+            _contents.push({id:this.max_content_id, title:_title, desc:_desc});
             this.setState({
-                contents: _contents
+                contents: _contents,
+                  mode:'read',
+          selected_content_id:this.max_content_id
             });
-            
-            console.log(_title, _desc);
         }.bind(this)}></CreateContent>
     }
       else if(this.state.mode === 'update'){
           _content =this.getReadContent();
-        _article = <UpdateContent data={_content} onSubmit={function(_title,_desc){
-            this.max_content_id = this.max_content_id+1;
-            var _contents = this.state.contents.concat(
-           {id:this.max_content_id, title:_title, desc:_desc}
-            )
+        _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+            var _contents = Array.from(this.state.contents);
+            var i = 0;
+            while(i < _contents.length){
+                if(_contents[i].id === _id){
+                    _contents[i] = {id:_id, title:_title, desc:_desc};
+                    break;
+                }
+                i =i+1;
+            }
+           
             this.setState({
-                contents: _contents
+                contents: _contents,
+                 mode:'read'
             });
-            
-            console.log(_title, _desc);
         }.bind(this)}></UpdateContent>
       }  
         return _article;
--- CRUD2/components/UpdateContent.js
+++ CRUD2/components/UpdateContent.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 
 
 class UpdateContent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        id:this.props.data.id,
+        title:this.props.data.title,
+        desc:this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
   render(){
     return (
       <article>
@@ -11,14 +23,25 @@ class UpdateContent extends Component{
           e.preventDefault();
         alert('test');
         this.props.onSubmit(
-        e.target.title.value,
-        e.target.desc.value    
+       this.state.id,
+        this.state.title,
+        this.state.value
         );
           }.bind(this)}
     >
-        <p><input type="text" name="title" placeholder="title"></input>        
+        <input type="hidden" name="id" value={this.state.id}></input> 
+        <p><input  
+            type="text" 
+            name="title" 
+            placeholder="title"
+            value={this.state.title}
+            onChange={this.inputFormHandler}></input>        
         </p>
-        <p><textarea name="desc"  placeholder="description"></textarea>  
+        <p><textarea 
+             onChange={this.inputFormHandler}
+             name="desc"  
+             placeholder="description" 
+             value={this.state.desc}></textarea>  
         </p>
         <p><input type="submit"></input>  
         </p>
